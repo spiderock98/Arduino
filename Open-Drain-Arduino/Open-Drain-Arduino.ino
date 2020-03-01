6f306908aa73791475e3893f2ff9114cac079ecf
NguyenMinhTien <spiderock@Nguyens-MacBook-Pro.local>
2020-03-01 09:57:12 +0700

    good morning

---
+++ Open-Drain-Arduino/Open-Drain-Arduino.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // Blink
+  pinMode(4, INPUT);
+  delay(500);
+  pinMode(4, OUTPUT);
+  digitalWrite(12, 0);
+  delay(500);
+}
